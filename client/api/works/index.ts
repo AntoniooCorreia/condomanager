7cc3b02881daa929ecb5a076adffb3032994e1be
AntoniooCorreia <antoniooc.net@gmail.com>
2026-04-04 15:55:55 +0100

    fix schema path to api root

--- client/api/works/index.ts
+++ client/api/works/index.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pkg from "pg";
-import { works } from "./schema";
+import { works } from "../schema";
 import { eq } from "drizzle-orm";
 
 const { Pool } = pkg;
