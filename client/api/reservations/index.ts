0b71813611ba030c75b7c598384bb8339a9a153a
AntoniooCorreia <antoniooc.net@gmail.com>
2026-04-04 15:47:02 +0100

    fix schema path duplicate client

--- client/api/reservations/index.ts
+++ client/api/reservations/index.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { drizzle } from "drizzle-orm/node-postgres";
 import pkg from "pg";
-import { reservations } from "../client/src/shared/schema";
+import { reservations } from "./src/shared/schema";
 import { eq } from "drizzle-orm";
 
 const { Pool } = pkg;
