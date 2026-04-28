21055d47beabe0358ccba63323bad4beab7f11db
AntoniooCorreia <antoniooc.net@gmail.com>
2026-04-29 00:06:39 +0100

    add Chat import to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,6 +23,7 @@ import { UserPagamentos } from "./pages/user/UserPagamentos";
 import { UserPerfil } from "./pages/user/UserPerfil";
 import { UserCameras } from "./pages/user/UserCameras";
 import { Avisos } from "./pages/shared/Avisos";
+import { Chat } from "./pages/shared/Chat";
 import { UserCobrancas } from "./pages/user/UserCobrancas";
 
 const AdminRoute = ({ component: Component }: { component: React.ComponentType }) => (
